be01ec272373653b5e0ea0889ab83d61d1cb5c1d
Anonatormachine <145568578+Anonatormachine@users.noreply.github.com>
2023-12-09 07:11:21 +0300

    Update V5_1.py

--- V5_1.py
+++ V5_1.py
@@ -2,19 +2,16 @@
 # Составить программу вычитания из первой дроби второй.
 # Ответ должен быть несократимой дробью. Использовать подпрограмму алгоритма Евклида для определения НОД.
 
-def euclid_algorithm(a, b): # Алгоритм Евклида для нахождения НОД
+def euclid_algorithm(a, b): 
     while b:
         a, b = b, a % b
     return a
 
-def subtract_fractions(A, B, C, D): # Вычитание дробей A/B - C/D
-    numerator = A * D - C * B # Находим числитель частного дробей путем умножение на знаменатели. A умножаем на знаменательно другой дроби
-    denominator = B * D # Находим общий знаменатель путем перемножения двух знаменателей
+def subtract_fractions(A, B, C, D): 
+    gcd = euclid_algorithm(numerator, denominator) 
 
-    gcd = euclid_algorithm(numerator, denominator) # Находим НОД для числителя и знаменателя
-
-    numerator //= gcd # Делим числитель на нод
-    denominator //= gcd # Делим знаменатель на нод
+    numerator //= gcd 
+    denominator //= gcd 
 
     return numerator, denominator
 
